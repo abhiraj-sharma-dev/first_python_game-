930d7c2111007f5be05fe020c2cf4f8e03394a81
abhiraj <abhirajmajhola2006@gmail.com>
2026-07-02 14:28:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import random 
+n =  random.randint(1,100)
+a = -1
+gusses = 1
+while(a != n):
+    a =  int(input("Guess the number: "))
+    if(a>n):
+        print("Lower number please")
+        gusses +=1
+
+    elif(a<n):
+        print("Higher number please")
+        gusses +=1
+
+print(f"You have Gussed the number {n} correctly in {gusses} attempts")
